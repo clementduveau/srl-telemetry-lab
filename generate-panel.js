b3110c3671e2e881e8ee33b5199d2d1a1620de56
Clément Duveau <clement@duveau.eu>
2024-05-24 12:54:26 +0200

    Generated panels

--- generate-panel.js
+++ generate-panel.js
@@ -1,74 +1,128 @@
-let elements = [
-  {
-    "background": {
-      "color": {
-        "fixed": "#D9D9D9"
-      }
+const fs = require('node:fs');
+
+const baseElement = {
+  "background": {
+    "color": {
+      "fixed": "#D9D9D9"
+    }
+  },
+  "border": {
+    "color": {
+      "fixed": "dark-green"
+    }
+  },
+  "constraint": {
+    "horizontal": "left",
+    "vertical": "top"
+  },
+  "type": "metric-value",
+  "config": {
+    "align": "center",
+    "color": {
+      "fixed": "#000000"
     },
-    "border": {
-      "color": {
-        "fixed": "dark-green"
-      }
+    "size": 12,
+    "text": {
+      //      "field": "leaf1:ethernet-1/1:out",
+      "fixed": "",
+      "mode": "field"
     },
-    "config": {
-      "align": "center",
-      "color": {
-        "fixed": "#000000"
-      },
-      "size": 12,
-      "text": {
-        "field": "leaf1:ethernet-1/1:out",
-        "fixed": "",
-        "mode": "field"
-      },
-      "valign": "middle"
-    },
-    "constraint": {
-      "horizontal": "left",
-      "vertical": "top"
-    },
-    "name": "leaf1-1-out",
-    "placement": {
-      "top": 566,
-      "height": 21,
-      "left": 87,
-      "width": 72
-    },
-    "type": "metric-value"
+    "valign": "middle"
+  },
+  //  "name": "leaf1-1-out",
+  "placement": {
+    // "top": 566,
+    "height": 21,
+    // "left": 87,
+    "width": 72
   }
+}
+
+let metrics = [
+  { source: 'leaf1', port: 1, dest: 'in' },
+  { source: 'leaf1', port: 1, dest: 'out' },
+  { source: 'leaf1', port: 49, dest: 'in' },
+  { source: 'leaf1', port: 49, dest: 'out' },
+  { source: 'leaf1', port: 50, dest: 'in' },
+  { source: 'leaf1', port: 50, dest: 'out' },
+  { source: 'leaf2', port: 1, dest: 'in' },
+  { source: 'leaf2', port: 1, dest: 'out' },
+  { source: 'leaf2', port: 49, dest: 'in' },
+  { source: 'leaf2', port: 49, dest: 'out' },
+  { source: 'leaf2', port: 50, dest: 'in' },
+  { source: 'leaf2', port: 50, dest: 'out' },
+  { source: 'leaf3', port: 1, dest: 'in' },
+  { source: 'leaf3', port: 1, dest: 'out' },
+  { source: 'leaf3', port: 49, dest: 'in' },
+  { source: 'leaf3', port: 49, dest: 'out' },
+  { source: 'leaf3', port: 50, dest: 'in' },
+  { source: 'leaf3', port: 50, dest: 'out' },
+  { source: 'spine1', port: 1, dest: 'in' },
+  { source: 'spine1', port: 1, dest: 'out' },
+  { source: 'spine1', port: 2, dest: 'in' },
+  { source: 'spine1', port: 2, dest: 'out' },
+  { source: 'spine1', port: 3, dest: 'in' },
+  { source: 'spine1', port: 3, dest: 'out' },
+  { source: 'spine2', port: 1, dest: 'in' },
+  { source: 'spine2', port: 1, dest: 'out' },
+  { source: 'spine2', port: 2, dest: 'in' },
+  { source: 'spine2', port: 2, dest: 'out' },
+  { source: 'spine2', port: 3, dest: 'in' },
+  { source: 'spine2', port: 3, dest: 'out' }
 ]
 
-let metrics= []
-
-const source=[
-  "leaf1",
-  "leaf2",
-  "leaf3",
-  "spine1",
-  "spine2"
+let elements = [
 ]
 
-const port = [
-  1,2,3,49,50
-]
+metrics.forEach(function (metric, i) {
+  let element = baseElement;
 
-const dest = [
-  "in",
-  "out"
-]
+  element.config.text.field = metric.source + ':ethernet-1/' + metric.port + ':' + metric.dest;
+  element.name = metric.source + '-' + metric.port + '-' + metric.dest;
 
-source.forEach(s => {
-  let el = { source: s };
-
-  port.forEach(p => {
-    el.port = p;
-
-    dest.forEach(d => {
-      el.dest = d;
-
-      metrics.push({...el});
-    });
-  })
+  // Spine on top
+  // Leaf on botom
+  let top = 0;
+  switch (metric.source) {
+    case 'spine1':
+      top = 50;
+      left = (160 + (metric.port * 80));
+      break;
+    case 'spine2':
+      top = 50;
+      left = (450 + (metric.port * 80));
+      break;
+    case 'leaf1':
+      top = 400;
+      if(metric.port == 1){top += 160;}
+      left = 80;
+      if(metric.port > 1){left += ((metric.port - 49)*80)}
+      break;
+    case 'leaf2':
+      top = 400;
+      if(metric.port == 1){top += 160;}
+      left = 300;
+      if(metric.port > 1){left += ((metric.port - 49)*80)}
+      break;
+    case 'leaf3':
+      top = 400;
+      if(metric.port == 1){top += 160;}
+      left = 660;
+      if(metric.port > 1){left += ((metric.port - 49)*80)}
+      break;
+    default:
+      throw new Error('Not supposed to reach default here');
+      break;
+  }
+  element.placement.top = top;
+  element.placement.left = left;
+  elements.push({ ...element })
 });
 
-console.log(metrics);
+fs.writeFile('./panel-result.json', JSON.stringify(elements), err => {
+  if (err) {
+    console.error(err);
+  } else {
+    // file written successfully
+  }
+});
